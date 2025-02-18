85e0f221c9ca2e881b072d5f8f2e63015fee9c35
RUCHIR04-R <ruchirgawture@gmail.com>
2025-02-18 10:47:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,36 @@
 function convertToRoman(num) {
-  const romanNumerals = [
-    ['M', 1000], 
-    ['CM', 900], 
-    ['D', 500], 
-    ['CD', 400], 
-    ['C', 100], 
-    ['XC', 90], 
-    ['L', 50], 
-    ['XL', 40], 
-    ['X', 10], 
-    ['IX', 9], 
-    ['V', 5], 
-    ['IV', 4], 
-    ['I', 1]
-  ];
+    const romanNumerals = [
+        ['M', 1000],
+        ['D', 500],
+        ['C', 100],
+        ['L', 50],
+        ['X', 10],
+        ['V', 5],
+        ['I', 1]
+    ];
 
-  let roman = '';
-
-  for (let [letter, value] of romanNumerals) {
-    while (num >= value) {
-      roman += letter;
-      num -= value;
+    let result = '';
+    
+    for (let [roman, value] of romanNumerals) {
+        while (num >= value) {
+            result += roman;
+            num -= value;
+        }
     }
-  }
-
-  return roman;
+    
+    return result;
 }
 
-function showRomanNumeral() {
-  const number = document.getElementById('numberInput').value;
-  if (number >= 0 && number <= 100000) {
-    const result = convertToRoman(parseInt(number));
-    document.getElementById('result').innerText = result;
-  } else {
-    document.getElementById('result').innerText = 'Please enter a number between 0 and 100000.';
-  }
-}
+document.getElementById('convertButton').addEventListener('click', () => {
+    const numberInput = document.getElementById('numberInput').value;
+    const resultElement = document.getElementById('result');
 
-// Example usage:
-console.log(convertToRoman(14)); // Output: XIV
-console.log(convertToRoman(798)); // Output: DCCXCVIII
-console.log(convertToRoman(100000)); // Output: (Historically, Roman numerals were not typically used for numbers this large, and modern conventions vary)
+    const number = parseInt(numberInput);
+    
+    if (number >= 0 && number <= 100000) {
+        const romanNumeral = convertToRoman(number);
+        resultElement.textContent = romanNumeral;
+    } else {
+        resultElement.textContent = 'Please enter a number between 0 and 100000.';
+    }
+});
