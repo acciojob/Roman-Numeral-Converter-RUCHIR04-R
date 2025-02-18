c5eb48f8947f03f9ff9063af5866643dbe56608a
RUCHIR04-R <ruchirgawture@gmail.com>
2025-02-18 10:41:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  const romanNumerals = [
+    ['M', 1000], 
+    ['CM', 900], 
+    ['D', 500], 
+    ['CD', 400], 
+    ['C', 100], 
+    ['XC', 90], 
+    ['L', 50], 
+    ['XL', 40], 
+    ['X', 10], 
+    ['IX', 9], 
+    ['V', 5], 
+    ['IV', 4], 
+    ['I', 1]
+  ];
 
-  //your code here
-let roman = '';
-	for(let i in obj)
-		{
-			while(num >= obj[i][1]) {
-				roman += obj[i][0]
-				num -= obj[i][1];
-			}
-		}
-	return roman;
+  let roman = '';
+
+  for (let [letter, value] of romanNumerals) {
+    while (num >= value) {
+      roman += letter;
+      num -= value;
+    }
+  }
+
+  return roman;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
-console.log(convertToRoman(36));
-// do not edit below this line
-module.exports = convertToRoman;
+function showRomanNumeral() {
+  const number = document.getElementById('numberInput').value;
+  if (number >= 0 && number <= 100000) {
+    const result = convertToRoman(parseInt(number));
+    document.getElementById('result').innerText = result;
+  } else {
+    document.getElementById('result').innerText = 'Please enter a number between 0 and 100000.';
+  }
+}
+
+// Example usage:
+console.log(convertToRoman(14)); // Output: XIV
+console.log(convertToRoman(798)); // Output: DCCXCVIII
+console.log(convertToRoman(100000)); // Output: (Historically, Roman numerals were not typically used for numbers this large, and modern conventions vary)
