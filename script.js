e18804551bf40abba6ec3286c16fcc7a2855c765
RUCHIR04-R <ruchirgawture@gmail.com>
2025-02-26 10:09:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
 function convertToRoman() {
-    const num = document.getElementById('numberInput').value;
-    const resultElement = document.getElementById('result');
-    if (num === '') {
-        resultElement.innerText = 'Please enter a number.';
+    var num = document.getElementById('numberInput').value;
+    var result = document.getElementById('result');
+    
+    if (isNaN(num) || num <= 0) {
+        result.innerText = 'Invalid Input';
         return;
     }
 
-    const romanNumeral = toRoman(num);
-    resultElement.innerText = romanNumeral;
-}
-
-function toRoman(num) {
-    const romanNumerals = [
-        ['M', 1000],
-        ['CM', 900],
-        ['D', 500],
-        ['CD', 400],
-        ['C', 100],
-        ['XC', 90],
-        ['L', 50],
-        ['XL', 40],
-        ['X', 10],
-        ['IX', 9],
-        ['V', 5],
-        ['IV', 4],
-        ['I', 1]
+    var roman = [
+        ["M", 1000],
+        ["CM", 900],
+        ["D", 500],
+        ["CD", 400],
+        ["C", 100],
+        ["XC", 90],
+        ["L", 50],
+        ["XL", 40],
+        ["X", 10],
+        ["IX", 9],
+        ["V", 5],
+        ["IV", 4],
+        ["I", 1]
     ];
 
-    let roman = '';
+    var converted = '';
 
-    for (const [letter, value] of romanNumerals) {
-        while (num >= value) {
-            roman += letter;
-            num -= value;
+    for (var i = 0; i < roman.length; i++) {
+        while (num >= roman[i][1]) {
+            converted += roman[i][0];
+            num -= roman[i][1];
         }
     }
 
-    return roman;
+    result.innerText = converted.toUpperCase();
 }
