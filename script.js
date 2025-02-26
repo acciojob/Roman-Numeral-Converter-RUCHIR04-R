94758609a6f6c10d5ec43ce288c131cecd6d0973
RUCHIR04-R <ruchirgawture@gmail.com>
2025-02-26 09:31:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
-function convertToRoman(num) {
+function convertToRoman() {
+    const num = document.getElementById('numberInput').value;
+    const resultElement = document.getElementById('result');
+    if (num === '') {
+        resultElement.innerText = 'Please enter a number.';
+        return;
+    }
+
+    const romanNumeral = toRoman(num);
+    resultElement.innerText = romanNumeral;
+}
+
+function toRoman(num) {
     const romanNumerals = [
         ['M', 1000],
+        ['CM', 900],
         ['D', 500],
+        ['CD', 400],
         ['C', 100],
+        ['XC', 90],
         ['L', 50],
+        ['XL', 40],
         ['X', 10],
+        ['IX', 9],
         ['V', 5],
+        ['IV', 4],
         ['I', 1]
     ];
 
-    let result = '';
-    
-    for (let [roman, value] of romanNumerals) {
+    let roman = '';
+
+    for (const [letter, value] of romanNumerals) {
         while (num >= value) {
-            result += roman;
+            roman += letter;
             num -= value;
         }
     }
-    
-    return result;
+
+    return roman;
 }
-
-document.getElementById('convertButton').addEventListener('click', () => {
-    const numberInput = document.getElementById('numberInput').value;
-    const resultElement = document.getElementById('result');
-
-    const number = parseInt(numberInput);
-    
-    if (number >= 0 && number <= 100000) {
-        const romanNumeral = convertToRoman(number);
-        resultElement.textContent = romanNumeral;
-    } else {
-        resultElement.textContent = 'Please enter a number between 0 and 100000.';
-    }
-});
